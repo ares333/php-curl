85e6da16580cf99c1f870cbdfe83f4a8020265d3
Ares <admin@phpdr.net>
2015-09-13 11:41:34 +0800

    update

--- demo/core/curl_error.php
+++ demo/core/curl_error.php
@@ -3,25 +3,24 @@ require '../../CurlMulti/Core.php';
 $url1 = 'http://badurl1';
 $url2 = 'http://badurl2';
 $curl = new CurlMulti_Core ();
-$curl->maxTry=1;
+$curl->maxTry = 1;
 $curl->opt [CURLOPT_CONNECTTIMEOUT] = 1;
 $curl->opt [CURLOPT_TIMEOUT] = 1;
+// cbFail golbal
+$curl->cbFail = 'cbFailGlobal';
 // cbFail for individual task
 $curl->add ( array (
 		'url' => $url1
-), null, 'cbFailTask' );
-// cbFail golbal
-$curl->cbFail = 'cbFailGlobal';
-$curl->add ( array (
+), null, 'cbFailTask' )->add ( array (
 		'url' => $url2
 ) );
 // start spider
 $curl->start ();
 function cbFailTask($err, $args) {
-	echo $err ['info'] ['url'] . "\n";
+	echo 'Task Fail: '.$err ['info'] ['url'] . "\n";
 	print_r ( $err ['error'] );
 }
 function cbFailGlobal($err, $args) {
-	echo $err ['info'] ['url'] . "\n";
+	echo 'Global Fail: '.$err ['info'] ['url'] . "\n";
 	print_r ( $err ['error'] );
 }
